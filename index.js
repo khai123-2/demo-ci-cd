a693a537bea83f8e6eb660444c2842e35b15e5b8
khai123-2 <tranquangkhai9102gmail.com>
2023-08-14 23:50:36 +0700

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/home", (req, res) => {
-  res.send("Hello");
+  res.send("Hello my friend");
 });
 
 app.listen(PORT, () => {
