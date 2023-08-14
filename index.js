42043b7d21961ec572dbd2073cef3debfd31be25
khai123-2 <tranquangkhai9102gmail.com>
2023-08-15 01:17:25 +0700

    fix index file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/home", (req, res) => {
-  res.send("Hello my friend");
+  res.send("Hello world");
 });
 
 app.listen(PORT, () => {
