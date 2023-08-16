a042e4b77e9824871c662409f67ad7122f3be029
khai123-2 <tranquangkhai9102gmail.com>
2023-08-16 15:01:17 +0700

    add method

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8081;
 
 app.get("/home", (req, res) => {
-  res.send("Hello world 567");
+  res.send("Hello world 789");
 });
 
 app.get("/", (req, res) => {
